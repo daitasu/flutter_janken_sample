cc60c321b2957cf9423813d222c49f9b97db9882
daitasu <ifctp.dai.monara@gmail.com>
2023-08-23 16:53:08 +0900

    add: Display photos like Instagram

--- 02_img_searcher/lib/main.dart
+++ 02_img_searcher/lib/main.dart
@@ -25,10 +25,11 @@ class ImgSearchPage extends StatefulWidget {
 
 class _ImgSearchPageState extends State<ImgSearchPage> {
   List hits = [];
+  String query = '花';
 
   Future<void> fetchImages() async {
     Response response = await Dio().get(
-        'https://pixabay.com/api/?key=38847204-143c9db58a89fb463b2edd56d&q=yellow+flowers&image_type=photo');
+        'https://pixabay.com/api/?key=38847204-143c9db58a89fb463b2edd56d&q=$query&image_type=photo');
     print(response.data['total']);
 
     hits = response.data['hits'];
@@ -44,13 +45,55 @@ class _ImgSearchPageState extends State<ImgSearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          initialValue: query,
+          decoration: const InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+          ),
+          onFieldSubmitted: (value) {
+            query = value;
+            fetchImages();
+          },
+        ),
+      ),
       body: GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 3),
           itemCount: hits.length,
           itemBuilder: (context, index) {
             Map<String, dynamic> hit = hits[index];
-            return Image.network(hit['previewURL']);
+            return InkWell(
+              onTap: () => {
+                print('${hit['likes']}'),
+              },
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.network(
+                    hit['previewURL'],
+                    fit: BoxFit.cover,
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(
+                            Icons.thumb_up_alt_outlined,
+                            size: 15.0,
+                          ),
+                          Text('${hit['likes']}'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
           }),
     );
   }
